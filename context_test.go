329a033120f69bc21e5b7ac71d565cae0de65ee3
Cattī Crūdēlēs <17695588+wzy9607@users.noreply.github.com>
2024-02-13 16:00:08 +0800

    add some unittest
    Signed-off-by: Cattī Crūdēlēs <17695588+wzy9607@users.noreply.github.com>

---
+++ context_test.go
@@ -0,0 +1,43 @@
+package amqp091otel
+
+import (
+	"context"
+	"testing"
+
+	"github.com/rabbitmq/amqp091-go"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContextFromDelivery(t *testing.T) {
+	t.Parallel()
+	ctxInst := context.WithValue(context.Background(), "key", "value")
+	type args struct {
+		msg amqp091.Delivery
+	}
+	tests := []struct {
+		name string
+		args args
+		want context.Context
+	}{
+		{
+			name: "instrumented context",
+			args: args{
+				msg: amqp091.Delivery{
+					Acknowledger: &acknowledger{ctx: ctxInst},
+				},
+			},
+			want: ctxInst,
+		}, {
+			name: "background context",
+			args: args{
+				msg: amqp091.Delivery{},
+			},
+			want: context.Background(),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, ContextFromDelivery(tt.args.msg), "ContextFromDelivery(%v)", tt.args.msg)
+		})
+	}
+}
